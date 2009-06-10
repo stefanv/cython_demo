1e15d051543b656a3e8f4de1a7e54cb247bf42a6
Stefan van der Walt <stefan@sun.ac.za>
2009-06-10 15:53:38 +0200

    More complete setup.py

--- setup.py
+++ setup.py
@@ -10,6 +10,18 @@ def cext(ext_name):
                      ['%s/%s.pyx' % (module, ext_name)],
                      include_dirs=[numpy.get_include()])
 
-setup(cmdclass = {'build_ext': build_ext},
-      ext_modules = [cext('calc')])
+setup(name=module,
+      version='0.0',
+      description='Calculator of Epic Proportion',
+      author='My Name',
+      author_email='myname@myname.org',
+      license='BSD',
+      url='http://myname.org/calc',
+
+      # -----
+
+      packages=[module],
+      cmdclass = {'build_ext': build_ext},
+      ext_modules = [cext('calc')]
+)
 
